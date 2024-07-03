c899d9c69810918db943fea3a5683cd76389327f
bilal1718 <muhbil171845@gmail.com>
2024-07-03 21:11:44 +0500

    My first Python Program

--- Python/Problem1.py
+++ Python/Problem1.py
@@ -1,1 +1,8 @@
-Problem1.p
+distanceOfTripInKilometers=int(input("Enter the distance of trip in kilometers: "))
+fuelEffiencyOfCar=float(input("Enter the fuel effiency of car (kilometers per liter) : "))
+costPerLiterOfFuel=float(input("Enter the cost per liter of fuel: "))
+
+totalFuelRequiredForTrip=distanceOfTripInKilometers * fuelEffiencyOfCar
+totalCostRequiredForTrip=totalFuelRequiredForTrip * costPerLiterOfFuel
+print("The total fuel required for the trip is: ", totalFuelRequiredForTrip)
+print("The total cost of the trip based on the fuel efficiency and cost per liter is: ", totalCostRequiredForTrip)
