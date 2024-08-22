45ad377df1ffa7e17bfcec7497e2ad42e014cb5d
Muhammad Bilal <muhbil171845@gmail.com>
2024-08-22 15:19:24 +0500

    Convert object into float using frequency encoding

--- ML_Projects/Customer_Insights_And_Prediction/sales_prediction_model.py
+++ ML_Projects/Customer_Insights_And_Prediction/sales_prediction_model.py
@@ -6,13 +6,17 @@ os.chdir("c:/Users/CT/Desktop/AI_Journey/ML_Projects/Customer_Insights_And_Predi
 df = pd.read_csv("sales_forecasting_dataset.csv")
 df.drop(columns=["Row ID","Order ID","Product Name","Customer ID","Customer Name","Country","Product ID","Postal Code"],inplace=True)
 
-print(df.dtypes)
-
 
 X=df.drop("Sales",axis=1)
 y=df["Sales"]
+cols_to_encode = X
+for col in cols_to_encode:
+    freq_encoding = X[col].value_counts() / len(X)
+    X[col] = X[col].map(freq_encoding)
 
+print("Input Features: ")
 print(X.head())
+print("Target Value: ")
 print(y.head())
 
 
