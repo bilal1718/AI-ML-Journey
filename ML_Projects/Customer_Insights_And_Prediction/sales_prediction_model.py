b8ea8c09801b3cddfac1b2b1b17c915065b11220
Muhammad Bilal <muhbil171845@gmail.com>
2024-08-22 13:12:28 +0500

    Separating Features and Target Variable

--- ML_Projects/Customer_Insights_And_Prediction/sales_prediction_model.py
+++ ML_Projects/Customer_Insights_And_Prediction/sales_prediction_model.py
@@ -4,9 +4,16 @@ import os
 
 os.chdir("c:/Users/CT/Desktop/AI_Journey/ML_Projects/Customer_Insights_And_Prediction/")
 df = pd.read_csv("sales_forecasting_dataset.csv")
-df.drop(columns=["Row ID","Order ID","Product Name","Customer ID","Customer Name","Country","Product ID"],inplace=True)
+df.drop(columns=["Row ID","Order ID","Product Name","Customer ID","Customer Name","Country","Product ID","Postal Code"],inplace=True)
 
-print(df.head())
+print(df.dtypes)
+
+
+X=df.drop("Sales",axis=1)
+y=df["Sales"]
+
+print(X.head())
+print(y.head())
 
 
 
