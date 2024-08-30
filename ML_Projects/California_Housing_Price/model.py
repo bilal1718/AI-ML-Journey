abd51deff04210c61759b17cbe25f1ad483f5ab5
Muhammad Bilal <muhbil171845@gmail.com>
2024-08-30 12:46:51 +0500

    Describing Data again

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housing_Price/housing.csv")
-
+print(data["ocean_proximity"].value_counts())
 print(data.head())
+print(data.describe())
