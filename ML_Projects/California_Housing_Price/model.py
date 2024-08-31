a603bdc194c7129f0de3a1d449b59ec8cac8ab3f
Muhammad Bilal <muhbil171845@gmail.com>
2024-08-31 16:28:03 +0500

    Studying first 20 examples

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housing_Price/housing.csv")
 print(data["ocean_proximity"].value_counts())
-print(data.head())
+print(data.head(20))
 print(data.describe())
