a56bb07ba9f606fa6f6c37a98afd1e6013e3e842
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-01 10:35:03 +0500

    Histograms for visualizing data

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -1,11 +1,23 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housing_Price/housing.csv")
-print(data["ocean_proximity"].value_counts())
-print(data.head(20))
-print(data.describe())
+# print(data["ocean_proximity"].value_counts())
+# print(data.head(20))
+# print(data.describe())
 
 
-data.hist(bins=50, figsize=(12,8))
-plt.show()
+# data.hist(bins=50, figsize=(12,8))
+# plt.show()
+
+def shuffle_and_split_data(data, test_ratio):
+    shuffled_indices=np.random.permutation(len(data))
+    test_set_size=int(len(data) * test_ratio)
+    test_indices=shuffled_indices[:test_set_size]
+    train_indices=shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set , test_set = shuffle_and_split_data(data, 0.2)
+len(train_set)
+len(test_set)
