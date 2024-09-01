79835e7e8e377bee9740035bbd96035613b42411
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-01 10:24:23 +0500

    Histograms for visualizing data

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housing_Price/housing.csv")
 print(data["ocean_proximity"].value_counts())
 print(data.head(20))
 print(data.describe())
+
+
+data.hist(bins=50, figsize=(12,8))
+plt.show()
