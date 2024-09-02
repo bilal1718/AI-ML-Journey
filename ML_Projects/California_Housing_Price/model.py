da72a207db90af765c2fb3ed659f8e85e16bf7aa
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-02 10:59:59 +0500

    Creating new column for categorical income

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -22,10 +22,17 @@ data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housin
 
 # train_set , test_set = shuffle_and_split_data(data, 0.2)
 
-
-
-
-
 from sklearn.model_selection import train_test_split
 
-train_set, test_set=train_test_split(data, test_set=0.2, random_state=42)
+train_set, test_set=train_test_split(data, test_size=0.2, random_state=42)
+
+data["income_cat"]=pd.cut(
+    data["median_income"],
+    bins=[0.,1.5,3.0,4.5,6.,np.inf],
+    labels=[1,2,3,4,5]
+)
+
+data["income_cat"].value_counts().sort_index().plot.bar(rot=0,grid=True)
+plt.xlabel("Income Category")
+plt.ylabel("Number of districts")
+plt.show()
