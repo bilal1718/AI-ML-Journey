521ad64f1ff51c0706cb68aff56ee77668739a3c
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-02 10:36:23 +0500

    Adding sklearn for reproducibility of sets on various runs

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -11,13 +11,21 @@ data = pd.read_csv("C:/Users/CT/Desktop/AI_Journey/ML_Projects/California_Housin
 # data.hist(bins=50, figsize=(12,8))
 # plt.show()
 
-def shuffle_and_split_data(data, test_size):
-    shuffled_indices= np.random.permutation(len(data))
-    test_set_size=int(len(data) * test_size)
-    test_indices=shuffled_indices[:test_set_size]
-    train_indices=shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
 
-train_set , test_set = shuffle_and_split_data(data, 0.2)
-print(len(train_set))
-print(len(test_set))
+ # Custom shuffling code
+# def shuffle_and_split_data(data, test_size):
+#     shuffled_indices= np.random.permutation(len(data))
+#     test_set_size=int(len(data) * test_size)
+#     test_indices=shuffled_indices[:test_set_size]
+#     train_indices=shuffled_indices[test_set_size:]
+#     return data.iloc[train_indices], data.iloc[test_indices]
+
+# train_set , test_set = shuffle_and_split_data(data, 0.2)
+
+
+
+
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set=train_test_split(data, test_set=0.2, random_state=42)
