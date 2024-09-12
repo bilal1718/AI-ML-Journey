376ebe1d9fc637b09965eb64cca3851f49e630c7
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-12 08:35:43 +0500

    complete custom transformer

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -186,3 +186,21 @@ def ratio_pipeline():
         FunctionTransformer(column_ratio, feature_names_out=ratio_name),
         StandardScaler()
     )
+log_pipeline=make_pipeline(
+    SimpleImputer(strategy="median"),
+    FunctionTransformer(np.log, feature_names_out="one-to-one"),
+    StandardScaler()
+)
+cluster_simil=ClusterSimilarity(n_clusters=10, gamma=1. , random_state=42)
+default_num_pipeline=make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
+
+preprocessing=ColumnTransformer([
+    ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+    ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+    ("people_per_house", ratio_pipeline(), ["population", "households"]),
+    ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population", "households", "median_income"]),
+    ("gep", cluster_simil, ["latitude", "longitude"]),
+    ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+],
+remainder=default_num_pipeline
+)
