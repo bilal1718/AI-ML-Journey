66b23a96992e2e86a3cbe7123ea0022bc2c23a99
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-15 15:44:02 +0500

    Define parameters

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -252,3 +252,9 @@ full_pipeline=Pipeline([
     ("preprocessing", preprocessing),
     ("random_forest", RandomForestRegressor(random_state=42)),
 ])
+param_grid=[
+    {'preprocessing__geo__n_clusters':[5,8,10],
+     'random_forest__max_features':[4,6,8]},
+    {'preprocessing__geo__n_clusters':[10,15],
+     'random_forest__max_features':[6,8,10]},
+]
