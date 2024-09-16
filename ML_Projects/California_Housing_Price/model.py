b16a858c321e8a800e97d3b2c8e2a268bc4c9a27
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-16 13:19:44 +0500

    Fitting the data

--- ML_Projects/California_Housing_Price/model.py
+++ ML_Projects/California_Housing_Price/model.py
@@ -272,4 +272,5 @@ param_distribs=[
     {'preprocessing__geo__n_clusters':randint(low=3, high=50),
      'random_forest__max_features':randint(low=2, high=20)},
 ]
-
+rnd_search=RandomizedSearchCV(full_pipeline, param_distributions=param_distribs, n_iter=10, cv=3, scoring="neg_root_mean_squared_error", random_state=42)
+rnd_search.fit(data, data_labels)
