127b783dec9f0f5abd4f7124d4d5ed1e2ae382c1
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-23 08:23:32 +0500

    Showing first digit

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -1,6 +1,15 @@
 from sklearn.datasets import fetch_openml
-
+import matplotlib.pyplot as plt
 mnist=fetch_openml('mnist_784', as_frame=False)
 X,y=mnist.data, mnist.target
 print(X.shape)
 print(y.shape)
+def plot_digit(image_data):
+    image=image_data.reshape(28,28)
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+some_digit=X[0]
+plot_digit(some_digit)
+plt.show()
+
