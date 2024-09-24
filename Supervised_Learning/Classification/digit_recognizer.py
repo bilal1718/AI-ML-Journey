a58fc7dc1184b9760d0125049392dee9d7dd2cc1
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-24 08:24:03 +0500

    Training data

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -19,3 +19,7 @@ X_train, X_test, y_train, y_test=X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5=(y_train=='5')
 y_test_5=(y_test=='5')
 
+from sklearn.linear_model import SGDClassifier
+
+sdg_clf=SGDClassifier(random_state=42)
+sdg_clf.fit(X_train, y_train_5)
