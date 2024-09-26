532181c7c80861c759f5c7071034505b220c453c
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-26 08:38:12 +0500

    predicting each class

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -49,3 +49,10 @@ dummy_clf.fit(X_train, y_train_5)
 cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 
 ############################################
+
+# Confusion Matrices
+
+from sklearn.model_selection import cross_val_predict
+
+y_train_pred=cross_val_predict(sdg_clf, X_train, y_train_5, cv=3)
+
