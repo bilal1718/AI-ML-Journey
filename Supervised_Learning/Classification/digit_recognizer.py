858f8040d23561f32797070d4dfdbc235ca874fd
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-27 18:28:13 +0500

    Adding perfect predictions

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -59,3 +59,6 @@ y_train_pred=cross_val_predict(sdg_clf, X_train, y_train_5, cv=3)
 from sklearn.metrics import confusion_matrix
 
 cm=confusion_matrix(y_train_5, y_train_pred)
+
+y_train_perfect_predictions=y_train_5
+confusion_matrix(y_train_5, y_train_perfect_predictions)
