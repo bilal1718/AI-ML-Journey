b1bf4a32724bf0bce222e3ffa080e59d3c7818b7
Muhammad Bilal <muhbil171845@gmail.com>
2024-09-29 13:18:17 +0500

    for achiving the 90% precision, set high threshold

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -85,3 +85,7 @@ print(f1)
 y_scores=sdg_clf.decision_function([some_digit])
 print(y_scores)
 y_scores=cross_val_predict(sdg_clf, X_train, y_train_5, cv=3, method="decision_function")
+
+y_train_pred_90 = (y_scores > 70000)
+precision_score(y_train_5, y_train_pred_90)
+recall_score(y_train_5, y_train_pred_90)
