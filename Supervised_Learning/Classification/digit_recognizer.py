b937064df94548f8ebd164a16db72cc1437311ae
Muhammad Bilal <muhbil171845@gmail.com>
2024-10-05 08:03:29 +0500

    Precision Recall curve

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -127,3 +127,5 @@ from sklearn.ensemble import RandomForestClassifier
 forest_clf=RandomForestClassifier(random_state=42)
 
 y_probas_forest=cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+y_scores_forest=y_probas_forest[:,1]
+precisions_forest, recalls_forest, thresholds_forest=precision_recall_curve(y_train_5, y_scores_forest)
