4ce5271d33072433c274fb44ae7aa63581f90857
Muhammad Bilal <muhbil171845@gmail.com>
2024-10-13 09:33:51 +0500

    Displaying Confusion Matrix

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -170,3 +170,13 @@ from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 X_train_scaled=scaler.fit_transform(X_train.astype("float64"))
 cross_val_score(sdg_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
+
+
+############################################
+
+# Error Analysis
+
+from sklearn.metrics import ConfusionMatrixDisplay
+y_train_pred=cross_val_predict(sdg_clf, X_train_scaled, y_train, cv=3)
+ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred)
+plt.show()
