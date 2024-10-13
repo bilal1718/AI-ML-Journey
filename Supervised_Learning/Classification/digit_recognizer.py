a4f55e1826b149bb0044f233d899164b192e0da6
Muhammad Bilal <muhbil171845@gmail.com>
2024-10-13 09:37:08 +0500

    Normalizing confusion matrix

--- Supervised_Learning/Classification/digit_recognizer.py
+++ Supervised_Learning/Classification/digit_recognizer.py
@@ -178,5 +178,5 @@ cross_val_score(sdg_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
 
 from sklearn.metrics import ConfusionMatrixDisplay
 y_train_pred=cross_val_predict(sdg_clf, X_train_scaled, y_train, cv=3)
-ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred)
+ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred, normalize="true", values_format=".0%")
 plt.show()
